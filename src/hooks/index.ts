c28d70986cd9e723d242d2103846cadacc2e70c3
Yuki123456788 <yuki11835@gmail.com>
2025-01-04 23:10:09 +0800

    feat: 新增 API 配置
    調整項目：
    - 串接 api

---
+++ src/hooks/index.ts
@@ -0,0 +1,9 @@
+import { useUploadImage, useUploadImages } from "./useUploadImage";
+import { useGetArticle } from "./useGetArticle";
+
+
+export {
+  useUploadImage,
+  useUploadImages,
+  useGetArticle,
+};
---
+++ src/hooks/useUploadImage.ts
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useMutation } from "react-query";
+import apiClient from "./apiClient";
+
+
+interface UploadImageParams {
+  image: File;
+}
+
+interface UploadImageResponse {
+  key: string;
+}
+
+const uploadImage = async (params: UploadImageParams): Promise<UploadImageResponse> => {
+  const reader = new FileReader();
+
+  const arrayBuffer = await new Promise<ArrayBuffer>((resolve, reject) => {
+    reader.onload = () => {
+      resolve(reader.result as ArrayBuffer);
+    };
+    reader.onerror = () => reject('讀取檔案失敗');
+    reader.readAsArrayBuffer(params.image);
+  });
+
+  const response = await apiClient.post('/uploadImage', arrayBuffer, {
+    headers: {
+      'Content-Type': params.image.type,
+    },
+  });
+  return { key: response.data.key };
+};
+
+export const useUploadImage = () => {
+  return useMutation(uploadImage);
+};
+
+
+export const useUploadImages = () => {
+  const [imageKeys, setImageKeys] = useState<string[]>([]);
+  const { mutateAsync, isLoading } = useUploadImage();
+
+  // 批次上傳圖片
+  const uploadImages = async (files: File[]) => {
+    const keys: string[] = [];
+
+    for (const file of files) {
+      try {
+        const { key } = await mutateAsync({ image: file });
+        keys.push(key);
+      } catch (error) {
+        console.error(`Error uploading ${file.name}:`, error);
+      }
+    }
+
+    // 更新 state
+    setImageKeys(keys);
+  };
+
+  return { imageKeys, uploadImages, isLoading };
+};
+
